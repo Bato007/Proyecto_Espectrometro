a510fbdb96eb990fb81c75e985b54bf0d4430733
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-11-15 13:15:52 -0600

    No es mucho :v

---
+++ main.py
@@ -0,0 +1,9 @@
+#_______________________________________________________
+# Universidad del Valle de Guatemala
+# FF20182020202 - Física 3
+# Andrea Amaya    | Brandon Hernández 
+# Oliver de León  | Laura Tamath
+# Proyecto Final 
+# Simulador del espectrómetro
+#_______________________________________________________
+
