8c71a6f710c1206a4c41cba0ec67ca272b8c69f8
Olivverde <dele19270@uvg.edu.gt>
2020-11-27 15:30:31 -0600

    GUI improvement

--- Physics.py
+++ Physics.py
@@ -1,10 +1,8 @@
 #_______________________________________________________
 # Universidad del Valle de Guatemala
-# CC3056 - Programacion de Microprocesadores
-# Martín Amado         | Andrea Amaya
-# Brandon Hernández    | Oliver de León  | Laura Tamath
-# Proyecto no. 2
-# Desencriptador de archivos
+# Laura Tamath         | Andrea Amaya
+# Brandon Hernández    | Oliver de León
+# Proyecto final
 #_______________________________________________________
 
 #Librerías utilizadas
@@ -13,225 +11,180 @@ from tkinter import messagebox
 from tkinter import ttk
 from tkinter import *
 from os import path
+import tkinter as tk
 from PIL import Image, ImageTk
 import os
-#import gmpy
+import func as f
 
-# LLAVE COMPLETA
-#\\]]^^__``aaHHIIJJKKZZ
-#00112233445566778899..
+#----------------G-U-I------------------------------------------------------------------
 
 # Interface
 root = Tk()
 root['bg'] = 'gray13'
-#Diseño del GUI
-root.title('Proyecto Física | Espectrómetro')
 
+# GUI's design
+root.title('Proyecto Física | Espectrómetro')
 root.call('wm', 'iconphoto', root._w, PhotoImage(file='atomo.png'))
 root.resizable(False,False)
 root.geometry('600x400')
 frame = Frame()
 frame.pack()
-#Título del GUI
+
+# GUI's tittle
 label = Label(root, text = 'SIMULADOR', font = ('Calibri Bold','40'), fg='cyan2', bg='gray13')
 label.place(x = 128, y = 5)
 
+# Just stuff
+particle_dicc = {' Proton':'protones@1.67E-27@1.67E-19',
+                 ' Neutron':'neutrones@1.67E-27@0',
+                 ' Electron':'electron@9.109E-31@-1.67E-19',
+                 ' Positron':'positron@9.11E-31@1.60E-19',
+                 ' Antimuon':'antimuon@1.89E-28@1.60e-19',
+                 ' Alpha':'particula alfa@6.64E-27@3.2e-19',
+                 ' Deuterium':'nucleo de deuterio@3.96E-30@1.602E-19',
+                 ' Ninguna':'nada@0@0'}
+variable = ''
+variable_2 = ''
+variable_3 = ''
+window = ''
 
-#Atributos
-route = ''
-keyRoute = ''
-line = ''
-key = 0
-    
-#Función para abrir archivos .txt
-def open_file():
-    global route
-    file = filedialog.askopenfilename(filetypes=[("Text files","*.txt")])
-    route = file
+#-----------------V-O-L-T-A-G-E---S-T-U-F-F-----------------------------------------------
 
-#Función para buscar la llave
-def key():
-    global keyRoute
-    global key
-    #Lectura del documento txt
-    keyRoute = filedialog.askopenfilename(filetypes=[("Text files","*.txt")])
-    file = open(keyRoute,'r')
-    key = file.readlines()
-    file.close
+# Reads voltage
+def voltageReader():
+    try:
+        temp_volt = int(vol_entry.get())
+        f.obtenerVoltaje(temp_volt)
+        messagebox.showinfo(message = 'VALOR ingresado con EXITO', title = 'Valor Ingresado')
+    except:
+        messagebox.showinfo(message = 'Ingrese un VALOR VALIDO', title = 'Error 404!')
+# Voltage command
+Button(text = 'INGRESAR', bg='turquoise4', fg='gray97', font = ('Calibri Bold','10'),
+       command = voltageReader).place(x = 400, y = 100)
+voltage_lbl = Label(root, text = 'Voltaje', font = ('Calibri Bold','12'),
+                    fg='white', bg='gray13').place(x = 100, y = 105)
+vol_entry = ttk.Entry()
+vol_entry.place(x=190, y=105, width=170)
 
-    if(key != []):
-        #comprueba si la clave ingresada es correcta o no, y emite un mensaje
-        if(key[0] == '18'):
-            key = int(key[0])
-            messagebox.showinfo(message = 'Llave Ingresada', title = 'Completo')
+#-----------------P-A-R-T-I-C-L-E---C-H-O-O-S-I-N-G---------------------------------------
+
+# Finish Selection
+
+def generatePlot():
+    f.speedSelector()
+    f.tracePath(f.getAllParticles())
+
+def finishSel():
+    if(f.voltage != ''):
+        if((variable.get() != ' Particula 1') & (variable_2.get() != ' Particula 2') & (variable_3.get() != ' Particula 3')):
+            # verifies txt existence
+            if (os.path.isfile("particulas.txt")):
+                os.remove("particulas.txt")
+            f= open("particulas.txt","w+")
+            f.write(particle_dicc[variable.get()]+'\n')
+            f.write(particle_dicc[variable_2.get()]+'\n')
+            f.write(particle_dicc[variable_3.get()])
+            f.close()
+            generatePlot()
+            messagebox.showinfo(message = 'VALORES ingresados con EXITO', title = 'Valor Ingresado')
+            window.withdraw()
         else:
-            messagebox.showinfo(message = 'Llave Erronea', title = 'Fatal')
-            key = 0
-    #Si el archivo no ha sido seleccionado, muestra una advertencia
+            messagebox.showinfo(message = 'Ingrese valores ADECUADOS', title = 'Fatal!!')
     else:
-        messagebox.showinfo(message = 'Archivo vacio', title = 'Contenido faltante')
-  
-#Función para leer el archivo a desencriptar
-def read():
-    if (route != ''):
-        if (key == 18):
-            global line
-            file = open(route,'r')
-            line = file.readlines()
-            file.close
-            #Desencripta el archivo, si todos los requerimientos están listos
-            if(line != []):
-                write(line)
-            #Muestra advertencias depenendiendo del requerimiento que falte
-            else:
-                messagebox.showinfo(message = 'Archivo vacio', title = 'Contenido faltante')
-        else:
-            messagebox.showinfo(message = 'Ingrese llave', title = 'Llave faltante')
-    else:
-        messagebox.showinfo(message = 'Se debe de seleccionar un archivo', title = 'Ruta faltante')
-
-#Función para enrutar el mensaje 
-def write(line):
-    final = []
-    x = line[0].split(',')
-    x.pop(len(x)-1)
-    for i in range(len(x)):
-        decrypted = ''
-        for j in range(len(x[i])):
-            decrypted += decrypt(x[i][j])
-        final.append(decrypted)
-    messagebox.showinfo(message = 'Indicar Ruta de Guardado', title = 'Requerimiento')
-    route = filedialog.asksaveasfilename(filetypes=[("Text files","*.txt")])
-    file = open(route,'w')
-    for i in range(len(final)):
-        file.write(str(final[i])+os.linesep)
-    file.close()
-    messagebox.showinfo(message = 'Desencriptado Finalizado', title = 'Completo')
-
-
-#Función para transcribir el mensaje desencriptado
-def decrypt(element):
-    decrypted = 0
-    element = ord(element)
-    a = element-97
-    #Utilizar aritmética modular inversa para desencriptar
-    #invmod = int(gmpy.divm(a,1,26))
-    if ((element < 98) & (element > 75)):
-        #decrypted = invmod - 26*2 + (97 - key)
-        decrypted = a + 26 - 26*2 + (97 - key)
-    else:
-        #decrypted = invmod - 26 + (97 - key)
-        decrypted = a + 26 - 26 + (97 - key)
-    decrypted = chr(decrypted)
-    #retornar el mensaje desencriptado
-    return decrypted
-
-#Función para asegurar que el programa se cerrará
-def closing():
-    if messagebox.askokcancel("Close program","¿Seguro que quiere salir del programa?"):
-        root.destroy()
+        messagebox.showinfo(message = 'Ingrese el VOLTAJE antes de continuar', title = 'Fatal!!')
     
-#Diseño y funcionamiento de los botones
-label_1 = Label(root, text = 'Voltaje', font = ('Calibri Bold','12'), fg='white', bg='gray13')
-label_1.place(x = 165, y = 104)
-
         
-entry_1 = ttk.Entry()
-entry_1.place(x=245, y=105, width=150)
+# Particle summoner
+def particleSummoner():
+    global variable, variable_2, variable_3, window
+    window = tk.Toplevel()
+    window['bg'] = 'gray14'
+    window.title('Choose particle') 
+    window.geometry('550x300')
+    
+    # label text for title 
+    ttk.Label(window, text = "PARTICULAS",  
+              background = 'gray14', foreground ="cyan3",  
+              font = ("Calibri Bold", 26)).place(x=175,y=0)
 
-#Multiple choice box-----------------------------------
-window = Tk()
-window['bg'] = 'gray14'
-window.title('Choose particle') 
-window.geometry('550x300')
-  
-# label text for title 
-ttk.Label(window, text = "PARTICULAS",  
-          background = 'gray14', foreground ="cyan3",  
-          font = ("Calibri Bold", 26)).grid(row = 0, column = 0)
-  
-# label 
-ttk.Label(window, text = "Selecciona una partícula :",
-          background = 'gray14', foreground ="white",  
-          font = ("Times New Roman", 12)).grid(column = 0, 
-          row = 5, padx = 10, pady = 25)
-  
-# Combobox creation 
-n = StringVar() 
-monthchoosen = ttk.Combobox(window, width = 27, textvariable = n) 
-  
-# Adding combobox drop down list 
-monthchoosen['values'] = (' Ninguna',
-                          ' Proton',  
-                          ' Neutron', 
-                          ' Electron', 
-                          ' Positron', 
-                          ' Tau', 
-                          ' Delta', 
-                          ' Alpha', 
-                          ' Deuterium') 
-  
-monthchoosen.grid(column = 1, row = 5)
-monthchoosen.current()
+#----------------FIRST PARTICLE---------------------
+    # label 
+    ttk.Label(window, text = "Primera particula",
+              background = 'gray14', foreground ="white",  
+              font = ("Times New Roman", 12)).place(x=50,y=70)
+      
 
-ttk.Label(window, text = "Selecciona la segunda partícula :",
-          background = 'gray14', foreground ="white",  
-          font = ("Times New Roman", 12)).grid(column = 0, 
-          row = 7, padx = 10, pady = 25)
-# Combobox creation 
-n = StringVar() 
-monthchoosen = ttk.Combobox(window, width = 27, textvariable = n) 
-  
-# Adding combobox drop down list 
-monthchoosen['values'] = (' Ninguna',
-                          ' Proton',  
-                          ' Neutron', 
-                          ' Electron', 
-                          ' Positron', 
-                          ' Tau', 
-                          ' Delta', 
-                          ' Alpha', 
-                          ' Deuterium') 
-  
-monthchoosen.grid(column = 1, row = 7)
-monthchoosen.current()
+    # Choice box
+    particles = (' Proton',  
+                ' Neutron', 
+                ' Electron', 
+                ' Positron', 
+                ' Antimuon', 
+                ' Alpha', 
+              ' Deuterium')
+    variable = StringVar(window)
+    variable.set(' Particula 1')
+    box_1 = OptionMenu(window,variable,*particles).place(x=60,y=100)
+    
+#----------------2nd PARTICLE---------------------
 
+    # label 
+    ttk.Label(window, text = "Segunda particula",
+              background = 'gray14', foreground ="white",  
+              font = ("Times New Roman", 12)).place(x=225,y=70)
+      
 
-ttk.Label(window, text = "Selecciona la tercera partícula :",
-          background = 'gray14', foreground ="white",  
-          font = ("Times New Roman", 12)).grid(column = 0, 
-          row = 9, padx = 10, pady = 25)
-# Combobox creation 
-n = StringVar() 
-monthchoosen = ttk.Combobox(window, width = 27, textvariable = n) 
-  
-# Adding combobox drop down list 
-monthchoosen['values'] = (' Ninguna',
-                          ' Proton',  
-                          ' Neutron', 
-                          ' Electron', 
-                          ' Positron', 
-                          ' Tau', 
-                          ' Delta', 
-                          ' Alpha', 
-                          ' Deuterium') 
-  
-monthchoosen.grid(column = 1, row = 9)
-monthchoosen.current()
+    # Choice box
+    particles_2 = (' Ninguna',
+                ' Proton',  
+                ' Neutron', 
+                ' Electron', 
+                ' Positron', 
+                ' Antimuon', 
+                ' Alpha', 
+              ' Deuterium')
+    variable_2 = StringVar(window)
+    variable_2.set(' Particula 2')
+    box_2 = OptionMenu(window,variable_2,*particles_2).place(x=240,y=100)
+    
 
+#----------------3rd PARTICLE---------------------
 
-#------------------------------------------------
-#Button(text = 'Abrir archivo', bg='black', fg='white', command = open_file).place(x = 275,y = 105)
-#Button(text = 'Desencriptar', bg='black', fg='white', command = read).place(x = 275,y = 195)
-Button(text = 'INGRESAR', bg='turquoise4', fg='gray97', font = ('Calibri Bold','10'),command = key).place(x = 255, y = 165)
+    # label 
+    ttk.Label(window, text = "Tercera particula",
+              background = 'gray14', foreground ="white",  
+              font = ("Times New Roman", 12)).place(x=400,y=70)
+      
+    # Choice box
+    particles_3 = (' Ninguna',
+                ' Proton',  
+                ' Neutron', 
+                ' Electron', 
+                ' Positron', 
+                ' Antimuon', 
+                ' Alpha', 
+              ' Deuterium')
+    variable_3 = StringVar(window)
+    variable_3.set(' Particula 3')
+    box_3 = OptionMenu(window,variable_3,*particles_3).place(x=400,y=100)
+
+#----------------Final Button---------------------
+    
+    Button(window,text = 'INGRESAR', bg='turquoise4', fg='gray97', font = ('Calibri Bold','10'),
+       command = finishSel).place(x = 240, y = 250)
+#----Ends func-----    
+    
+# Summons particle choosing window
+Button(text = 'Escoger Partículas', bg='turquoise4', fg='gray97', font = ('Calibri Bold','10'),
+       command = particleSummoner).place(x = 100, y = 160)
+        
+        
 
 #Imagen de GUI
 img = Image.open("particle.jpg")
 img = img.resize((185,130), Image.ANTIALIAS)
 my_image =  ImageTk.PhotoImage(img)
 lbl = Label(image = my_image).place(x=229,y=225)
-
-
 root.protocol("WM_DELETE_WINDOW", closing)
 
 
